c8840db577d2dcf1b9dcb02f171a5eeb30be7c74
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-06-27 23:13:04 -0300

    feat: adição do react

--- mylib/src/App.js
+++ mylib/src/App.js
@@ -1,9 +1,8 @@
-import "./App.css";
-
+import {BrowserRouter} from "react-router-dom";
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <BrowserRouter></BrowserRouter>
     </>
   );
 }
