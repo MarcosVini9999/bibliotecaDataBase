5ed8a2e5a1f7c1b655bd453966b3ec4d082752ad
MarcosVini9999 <marcosviniciusandradedesousa@hotmail.com>
2022-06-29 23:13:16 -0300

    style: tela de login

--- mylib/src/pages/home/index.js
+++ mylib/src/pages/home/index.js
@@ -1,4 +1,11 @@
+import {Link} from "react-router-dom";
 function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Link exact to="/">
+        Logout
+      </Link>
+    </>
+  );
 }
 export default Home;
--- mylib/src/pages/login/index.js
+++ mylib/src/pages/login/index.js
@@ -1,4 +1,28 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import {Link} from "react-router-dom";
+import "./style.css";
+
 function Login() {
-  return <h1>Tela de login</h1>;
+  return (
+    <>
+      <div className="containerBox">
+        <div className="boxLogin">
+          <h1 className="loginTitle">LOGIN</h1>
+          <div className="containerInput">
+            <TextField id="standard-basic" label="E-mail" variant="standard" />
+          </div>
+          <div className="containerInput">
+            <TextField id="standard-basic" label="Senha" variant="standard" />
+          </div>
+          <div>
+            <Link exact to="/home">
+              <Button variant="contained">ENTRAR</Button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 export default Login;
